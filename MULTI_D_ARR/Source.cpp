495d56e8879cfcbc515be8aa48a54cd6a7062455
DEV <dev@step.rivne>
2023-09-18 12:01:21 +0300

    Classwork

--- MULTI_D_ARR/Source.cpp
+++ MULTI_D_ARR/Source.cpp
@@ -51,21 +51,21 @@ void main() {
 		}cout << "\n";*/
 		/*Дано масив розміром 7×3 з елементами цілого типу.
 		Визначити кількість елементів, модуль яких менший 12. rand()%24- 12*/
-	/*const int rows = 7;
-	const int cols = 3;
-	int arr[rows][cols]{};
-	int counter = 0;
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++) {
-			arr[i][j] = -20 + rand() % 40; cout << setw(5) << arr[i][j] << " ";
-			if (arr[i][j] > -12 || arr[i][j]< 12) {
-				counter++;
-			}
+	//const int rows = 7;
+	//const int cols = 3;
+	//int arr[rows][cols]{};
+	//int counter = 0;
+	//for (int i = 0; i < rows; i++)
+	//{
+	//	for (int j = 0; j < cols; j++) {
+	//		arr[i][j] = -20 + rand() % 40; cout << setw(5) << arr[i][j] << " ";
+	//		if (arr[i][j] > -12 && arr[i][j]< 12) {
+	//			counter++;
+	//		}
 
-		}cout << "\n";
-	}
-	cout << counter;*/
+	//	}cout << "\n";
+	//}
+	//cout << counter;
 	/*.Дано масив розміром 4×5 з елементами цілого типу (позитивні та від’ємні). 
 	Визначити кількість позитивних елементів.*/
 	/*const int rows = 4;
@@ -85,21 +85,21 @@ void main() {
 	cout << counter; */
 	/*Дано масив розміром 5×4 з елементами дійсного типу (додатні та від’ємні). 
 	Визначити добуток всіх додатніх елементів.*/
-	//const int rows = 4;
-	//const int cols = 5;
-	//float arr[rows][cols]{};
-	//double counter=1;
-	//for (int i = 0; i < rows; i++)
-	//{
-	//	for (int j = 0; j < cols; j++) {
-	//		arr[i][j] =( - 200 + rand() % 400)/10.0; cout << setw(5) << arr[i][j] << " ";
-	//		if (arr[i][j] > 0) {
-	//			counter *= arr[i][j];
-	//		}
+	/*const int rows = 4;
+	const int cols = 5;
+	float arr[rows][cols]{};
+	double counter=1;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++) {
+			arr[i][j] =( - 200 + rand() % 400)/10.0; cout << setw(5) << arr[i][j] << " ";
+			if (arr[i][j] > 0) {
+				counter *= arr[i][j];
+			}
 
-	//	}cout << "\n";
-	//}
-	//cout << counter;
+		}cout << "\n";
+	}
+	cout << counter;*/
 	/*.Дано масив розміром 5×4 з елементами дійсного типу (позитивні та від’ємні). 
 	Визначити добуток всіх від’ємних елементів.*/
 	//const int rows = 4;
